b99f73ecc93f21bd89f189731c548e9d8565531c
henstel <henstel2013@gmail.com>
2021-09-05 21:16:01 +0100

    pass state data to props & using it in TodosList

---
+++ src/components/TodosList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodosList extends Component {
+
+    render() { 
+        return ( 
+            <ul>
+                {this.props.todos.map(todo => (<li key={todo.id}>{todo.title}</li>))}
+            </ul>
+         );
+    }
+}
+ 
+export default TodosList;
