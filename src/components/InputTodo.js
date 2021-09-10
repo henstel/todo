53b503e5cc76c58c8703b6e8e23d65ca6dd21f84
henstel <henstel2013@gmail.com>
2021-09-10 15:09:18 +0100

    modifying the onChange with setState

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -5,10 +5,16 @@ class InputTodo extends Component {
         title: ""
     };
 
+    onChange = e => {
+        this.setState({
+            title: e.target.value
+        })
+    };
+
     render() { 
         return (
             <form>
-                <input type="text" placeholder="Add Todo..." value={this.state.title} />
+                <input onChange={this.onChange} type="text" placeholder="Add Todo..." value={this.state.title} />
                 <button>Submit</button>
             </form>
           );
