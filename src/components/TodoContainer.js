19dff3778a117b164f50e7b1cd20773f92afedf2
henstel <henstel2013@gmail.com>
2021-10-22 13:42:07 +0100

    updating the state using setState

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -59,7 +59,7 @@ class TodoContainer extends React.Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ todos: data }));
   }
   
   render() {
