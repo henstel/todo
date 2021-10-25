1d114b328f35afc8191896ef40ef6866da71d6ec
henstel <henstel2013@gmail.com>
2021-10-25 12:39:40 +0100

    using useState hook to add local state in a functi

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,43 +1,7 @@
-import React, { Component } from 'react';
-
-class InputTodo extends Component {
-    state = {
-        title: ""
-    };
-
-    onChange = e => {
-        this.setState({
-            title: e.target.value
-        })
-    };
-
-    handleSubmit = e => {
-        e.preventDefault();
-        if (this.state.title.trim()) {
-            this.props.addTodoProps(this.state.title);
-            this.setState({
-            title: "",
-            })
-        } else {
-            alert("Please write item")
-        }
-    };
-    
-    render() { 
-        return (
-            <form onSubmit={this.handleSubmit} className="form-container">
-                <input 
-                    type="text"
-                    className="input-text"
-                    placeholder="Add Todo..." 
-                    value={this.state.title}
-                    name="title"
-                    onChange={this.onChange}
-                />
-                <button className="input-submit">Submit</button>
-            </form>
-          );
-    }
+import React, { useState } from "react"
+const InputTodo = () => {
+  console.log(useState("hello"))
+  return <div></div>
 }
- 
-export default InputTodo;
+
+export default InputTodo
