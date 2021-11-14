cb8b7e81386c482b753387eba0dcc1630e77a037
henstel <henstel2013@gmail.com>
2021-11-14 18:55:44 +0100

    import { BrowserRouter as Router }

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import ReactDOM from "react-dom"
 import TodoContainer from "./functionBased/components/TodoContainer"
 //stylesheet
 import "./functionBased/App.css"
+import { BrowserRouter as Router } from "react-router-dom"
 
-
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <TodoContainer />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById("root")
+  )  
