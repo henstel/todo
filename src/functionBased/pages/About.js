d0313003983ae7390ddfbab1116ccaf31cb1d207
henstel <henstel2013@gmail.com>
2021-11-21 01:03:10 +0100

    useparams and nested route

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,6 +1,29 @@
 import React from "react"
+import { Link, useMatch, Route, Routes, Outlet, useParams } from "react-router-dom"
+import SinglePage from "./SinglePage"
 
-const About = () => {
-  return <div>hello from about page</div>
+const About = () => { 
+  const { slug } = useParams();
+  console.log('slug =', slug)
+  
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={`about-app`}>About App</Link>
+        </li>
+        <li>
+          <Link to={`about-author`}>About Author</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path={`:slug`} element={<SinglePage />} />
+        
+      </Routes>
+      <Outlet />
+    </div>
+  )
+
+ 
 }
 export default About
