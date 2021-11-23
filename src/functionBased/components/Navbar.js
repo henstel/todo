b0becfe5956cdf5dcfee2759d2beb86ddab78f91
henstel <henstel2013@gmail.com>
2021-11-23 11:40:44 +0100

    styling the close/open button

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { NavLink, Outlet } from "react-router-dom"
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
 import "../App.css"
 
 
@@ -26,7 +28,14 @@ const handleToggle = () => {
    
   return (
     <nav className="navBar">
-      <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
+      <button onClick={handleToggle}>
+        {navbarOpen ? (
+        <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+        ) : (
+        <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+        )}
+      </button>
+
       <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
         {links.map(link => {
           return (
